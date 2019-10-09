681f280864988d0baaead9a3a0d04e421abeba24
Jason Lan <jason.lan@underarmour.com>
2019-10-09 13:21:27 -0700

    Updates

---
+++ precache-manifest.aec9e0b547cfe2b9a941ca35325a68ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ddbfb206f4aced4abb4b865ed2bb6c",
+    "url": "/CRA-gh-pages/index.html"
+  },
+  {
+    "revision": "5a6c5ba2b64d9707e6be",
+    "url": "/CRA-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3fc40e406e313c8d1d0b",
+    "url": "/CRA-gh-pages/static/js/2.9dff47ea.chunk.js"
+  },
+  {
+    "revision": "5a6c5ba2b64d9707e6be",
+    "url": "/CRA-gh-pages/static/js/main.3e449574.chunk.js"
+  },
+  {
+    "revision": "f9f4145896fee745cf24",
+    "url": "/CRA-gh-pages/static/js/runtime-main.c583dc4c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/CRA-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
